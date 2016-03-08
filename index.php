83982305a856bd3d320a9f2ee10fc8a82f85ef7d
pimpisa <pimpisa.predaswad@gmail.com>
2016-03-07 22:23:28 -0500

    added meta tag

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title> Parallax Scrolling using Javascript</title>
+		<title> Portfolio </title>
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script type="text/javascript" src="js/parallax.js"></script>	
 		<link href="css/style.css" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div class="content">
