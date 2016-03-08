148bca20f5ccf867a3b7d1081ace8389744678dd
pimpisa <pimpisa.predaswad@gmail.com>
2016-03-07 22:27:59 -0500

    included header & footer

--- include/header.php
+++ include/header.php
@@ -11,16 +11,3 @@
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-	<?php include(include/header.php)?>
-		<div class="content">
-			<img id="image" src="images/bg1.jpg">
-		</div>
-		<div class="content">
-		    <img id="image" src="images/bg2.jpg">
-		</div>
-		<div class="content">
-		    <img id="image" src="images/bg3.jpg">
-		</div>
-	<?php include(include/footer.php)?>
-	</body>
-</html>
