b39fca911057ee3bd6f973a6426836a67d26a61b
pimpisa <pimpisa.predaswad@gmail.com>
2016-03-07 22:12:13 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title> Parallax Scrolling using Javascript</title>
+		<script type="text/javascript" src="js/parallax.js"></script>	
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<img id="image" src="images/bg1.jpg">
+		<div id="content">
+		    <img id="image" src="images/bg2.jpg">
+		</div>
+	</body>
+</html>
